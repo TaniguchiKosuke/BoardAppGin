2bfebd768b9dc1a9cfc6a07f2c973a9dc25c83bf
KosukeTaniguchi <kosuke@KosukeTaniguchinoMacBook-Air.local>
2021-12-29 16:02:14 +0900

    create getAllBoardComent func and createBoardComment func

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func dbInit() {
@@ -19,7 +19,7 @@ type Board struct {
 	gorm.Model
 	ID       string `gorm:"primaryKey"`
 	Title    string
-	Comments []Comment `gorm:"foreignkey:BoardID"`
+	Comments []Comment `gorm:"foreignKey:BoardID"`
 }
 
 func getAllBoards() []Board {
@@ -49,6 +49,32 @@ func createBoard(title string) {
 	db.Create(&Board{ID: id, Title: title, Comments: nil})
 }
 
+func createBoardComment(boardId string, content string) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("cannot create comment")
+	}
+
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		panic("cannot generate uuid")
+	}
+
+	id := uuid.String()
+	db.Create(&Comment{ID: id, BoardID: boardId, Content: content})
+}
+
+func getAllBoardComments(boardId string) []Comment {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("cannot get comments")
+	}
+
+	var comments []Comment
+	db.Table("comments").Select("COALESCE(BoardID,?)", boardId).Find(&comments)
+	return comments
+}
+
 type Comment struct {
 	gorm.Model
 	ID      string `gorm:"primaryKey"`
@@ -63,21 +89,37 @@ func main() {
 
 	dbInit()
 
-	router.GET("/", func(ctx *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		allBoards := getAllBoards()
-		ctx.HTML(200, "index.html", gin.H{
+		c.HTML(200, "index.html", gin.H{
 			"boards": allBoards,
 		})
 	})
 
-	router.GET("new/board/", func(ctx *gin.Context) {
-		ctx.HTML(200, "create_board.html", gin.H{"title": "new board"})
+	router.GET("/new/board", func(c *gin.Context) {
+		c.HTML(200, "create_board.html", gin.H{"title": "new board"})
 	})
 
-	router.POST("new/board/post/", func(ctx *gin.Context) {
-		title := ctx.PostForm("title")
+	router.POST("/new/board/post", func(c *gin.Context) {
+		title := c.PostForm("title")
 		createBoard(title)
-		ctx.Redirect(302, "/")
+		c.Redirect(302, "/")
+	})
+
+	router.GET("/board/:id", func(c *gin.Context) {
+		boardId := c.Param("id")
+		allBoardComments := getAllBoardComments(boardId)
+		c.HTML(200, "board.html", gin.H{
+			"ID":       boardId,
+			"comments": allBoardComments,
+		})
+	})
+
+	router.POST("/board/:id/comment", func(c *gin.Context) {
+		content := c.PostForm("content")
+		boardId := c.Param("id")
+		createBoardComment(boardId, content)
+		c.Redirect(302, "/board/"+boardId)
 	})
 
 	router.Run()
